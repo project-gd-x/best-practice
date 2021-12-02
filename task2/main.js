397eafca4e32459eecaff2619b694eba547c22ed
Valerii Tanachov <valerii.tanachov@socialtechnologies.io>
2021-12-02 09:52:29 +0200

    feat: add task 2

---
+++ task2/main.js
@@ -0,0 +1,66 @@
+let box = document.getElementById('box');
+let inputColor = document.getElementById('inputColor');
+let btnRandom = document.getElementById('btnRandom');
+let btnApply = document.getElementById('btnApply');
+let btnReset = document.getElementById('btnReset');
+let letters = '0123456789ABCDEF';
+let defaultColor = '#969992';
+let arrColors = ['#FBCEB1', '#7FFFD4', '#E32636', '#9966CC', '#44944A', '#A8E4A0', '#4285B4', '#755A57', '#FFB02E', '#4169E1'];
+let divWrapper = document.getElementById('blockColors');
+let btnRandomAnimation = document.getElementById('btnRandomAnimation');
+let colorPreview = document.getElementById('colorPreview');
+let timer;
+
+box.style.backgroundColor = defaultColor;
+
+function getRandomColor() {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+btnRandom.addEventListener('click', function () {
+    inputColor.value = getRandomColor();
+});
+
+btnApply.addEventListener('click', function () {
+    if (inputColor.value !== '') {
+        box.style.backgroundColor = inputColor.value;
+    }
+});
+
+for (let i = 0; i < arrColors.length; i++) {
+    let colors = '';
+    colors = arrColors[i];
+
+    let innerDiv = document.createElement('div');
+    innerDiv.style.backgroundColor = colors;
+    innerDiv.addEventListener('click', function (e) {
+        // console.log('click1', innerDiv.style.backgroundColor);
+        // console.log('click2', e.target.style.backgroundColor);
+
+        inputColor.value = innerDiv.style.backgroundColor;
+
+    });
+    divWrapper.appendChild(innerDiv);
+}
+
+btnReset.addEventListener('click', function () {
+    inputColor.value = '';
+    box.style.backgroundColor = defaultColor;
+});
+
+btnRandomAnimation.addEventListener('click', function () {
+    timer = setInterval( function() {
+        colorPreview.style.backgroundColor = getRandomColor();
+        inputColor.value = colorPreview.style.backgroundColor;
+    }, 500);
+    
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    setTimeout(stopTimer, 5000);
+});
