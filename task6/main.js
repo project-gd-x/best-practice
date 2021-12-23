8f3ae7abba333320cc32a356fe423f79fd1b1b3d
Valerii Tanachov <valerii.tanachov@socialtechnologies.io>
2021-12-23 12:19:49 +0200

    fix: task6

--- task6/main.js
+++ task6/main.js
@@ -4,21 +4,14 @@ let snowLetter = '*';
 let windowsWidth = window.screen.width;
 let windowsHeight = window.screen.height;
 
-function initSnow() {
-    for (let i = 0; i <= count; i++) {
-        let posX = Math.floor(windowsWidth * Math.random());
-        let posY = Math.floor(windowsHeight * Math.random());
-
-        let myDiv = document.createElement('div');
-        myDiv.classList.add('snow');
-        myDiv.innerHTML = snowLetter;
-        myDiv.style.left = posX + 'px';
-        myDiv.style.top = posY + 'px';
-    }
-}
-
 for (let i = 0; i <= count; i++) {
+    let posX = Math.floor(windowsWidth * Math.random());
+    let posY = Math.floor(windowsHeight * Math.random());
+
+    let myDiv = document.createElement('div');
+    myDiv.classList.add('snow');
+    myDiv.innerHTML = snowLetter;
+    myDiv.style.left = posX + 'px';
+    myDiv.style.top = posY + 'px';
     document.body.appendChild(myDiv);
 }
-
-initSnow();
