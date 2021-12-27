3b7bb77d217a7f4be11befd44c7e6c719810eed8
Valerii Tanachov <valerii.tanachov@socialtechnologies.io>
2021-12-27 09:17:10 +0200

    fix: task6

--- task6/main.js
+++ task6/main.js
@@ -1,15 +1,16 @@
-const count = 5;
+const COUNT = 5;
 let snowLetter = '*';
 
 // let windowsWidth = window.screen.width - внутреняя ширина браузера;
 // window.innerWidth - ширина окна
 // document.documentElement.clientWidth - ширина html
 // document.body.clientWidth - ширина body
-let windowsWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+let windowsWidth = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) - 13;
 // let windowsHeight = window.screen.height;
-let windowsHeight = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+let windowsHeight = (window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight) - 37;
 
-for (let i = 0; i < count; i++) {
+for (let i = 0; i < COUNT; i++) {
+
     let posX = Math.floor(windowsWidth * Math.random());
     let posY = Math.floor(windowsHeight * Math.random());
 
