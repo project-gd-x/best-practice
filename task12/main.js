72d7a72b2b17eb109fd80634322fa44b832ea2e4
Valerii Tanachov <valerii.tanachov@socialtechnologies.io>
2022-08-25 18:08:30 +0300

    feat: task12

--- task12/main.js
+++ task12/main.js
@@ -13,6 +13,11 @@ let data = [
     }
 ]
 
+let dataJson = localStorage.getItem('data');
+if (dataJson !== null) {
+    data = JSON.parse(dataJson);
+}
+
 let nav = document.getElementById('nav');
 let inputText = document.getElementById('title');
 let textContent = document.getElementById('textContent');
@@ -21,30 +26,67 @@ let btnSave = document.getElementById('btnSave');
 let btnDelete = document.getElementById('btnDelete');
 let index = 0;
 let hasChanges = false;
+const regexp = /[^-A-Za-zА\d]/g;
+
+function resolveHash() {
+    const urlHash = location.hash; // #document-2
+
+    console.log('urlHash', urlHash);
+    for (let i = 0; i < data.length; i++) {
+        const title = data[i].title;
+        const dataHash = '#' + title.toLowerCase().split(' ').join('-').replace(regexp, ''); // #document-2
+        console.log('dataHash', dataHash);
+
+        if (urlHash === dataHash) {
+            index = i;
+            break;
+        }
+    }
+}
+resolveHash();
+
+
+window.addEventListener('hashchange', () => {
+    resolveHash();
+    render();
+})
 
 function getElementIndex (element) {
     return Array.from(element.parentNode.children).indexOf(element);
 }
 
+function replaceTitle (title) {
+    return  title
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
+
 function render() {
     nav.innerHTML = '';
+
     for (let i = 0; i < data.length; i++) {
         let items = document.createElement('li');
+
         if ( index === i && hasChanges ) {
-            items.innerHTML = '* ' + data[i].title;
+            items.innerHTML = '* ' + replaceTitle(data[i].title);
         } else {
-            items.innerHTML = data[i].title;
+            items.innerHTML = replaceTitle(data[i].title);
         }
+
         nav.appendChild(items);
 
         items.addEventListener('click', function () {
             inputText.value = data[i].title;
             textContent.value = data[i].text;
-
             let current = document.getElementsByClassName("active");
 
             current[0].className = current[0].className.replace("active", "");
             this.className += "active";
+
+            let formatUrl = data[i].title;
+            let url = formatUrl.toLowerCase().split(' ').join('-').replace(regexp, '');
+            window.history.pushState('object or string', 'Title', '#' + url);
+
             index = getElementIndex(current[0]);
 
             if (!data[index].isNew) {
@@ -67,17 +109,20 @@ render();
 
 btnDelete.addEventListener('click', function () {
 
-        if (data.length > 1) {
-            if (confirm('Вы уверены что хотите удалить этот пункт?')) {
-                data.splice(index, 1);
-            }
-        } else {
-            alert('Последний пункт не удаляется!');
+    if (data.length > 1) {
+        if (confirm('Вы уверены что хотите удалить этот пункт?')) {
+            data.splice(index, 1);
         }
+    } else {
+        alert('Последний пункт не удаляется!');
+    }
 
-        if (data[index] === undefined) {
-            index--
-        }
+    if (data[index] === undefined) {
+        index--
+    }
+
+    localStorage.removeItem('title');
+    localStorage.removeItem('text');
 
     render();
 });
@@ -85,6 +130,8 @@ btnDelete.addEventListener('click', function () {
 btnSave.addEventListener('click', function (event) {
     // data.splice(index, 1, {title: inputText.value, text: textContent.value});
     data[index] = {title: inputText.value, text: textContent.value, isNew: false};
+
+    localStorage.setItem('data', JSON.stringify(data));
 
     render();
 });
